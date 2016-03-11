fde4b416110d701bb8ad934653ce4bb26a648229
soulkeeper131 <vladimir.jotov@gmail.com>
2016-03-11 17:20:52 +0200

    Modul2 добавяне на коментари

---
+++ modul2/modul2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace modul2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			//Дефиниране на променливи
+
+			//Събиране на числа
+
+			//Писане в конзола
+		}
+	}
+}
